c6a1c764bd3c6d776c0ff27d577671f4482161a8
mikeumus <mikeumus@gmail.com>
2018-08-22 10:40:27 -0400

    Adding back required .gradle files.

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,10 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-<<<<<<< HEAD
-  public static final String DEVELOPMENT_URL = "expe76749b736ef4d49969b816375ce492c://192.168.43.8:19000";
-=======
   public static final String DEVELOPMENT_URL = "expe76749b736ef4d49969b816375ce492c://10.235.128.85:19000";
->>>>>>> a6caea08fbafcfffea7c5703373d70b512ab7575
 
 }
